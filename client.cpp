85a300a926816caa053ae86acaba3817160b7b42
basakest <ab1652759879@gmail.com>
2024-10-28 16:04:07 +0800

    code about protocol parsing

--- client.cpp
+++ client.cpp
@@ -5,8 +5,16 @@
 #include <unistd.h>
 #include <cstring>
 #include <cerrno>
+#include <cstring>
+#include <cassert>
 
+const size_t k_max_msg = 4096;
+
+static void msg(const char *msg);
 static void die(const char *msg);
+static int32_t query(int fd, const char *text);
+static int32_t write_all(int fd, const char *buf, size_t n);
+static int32_t read_full(int fd, char *buf, size_t n);
 
 int main()
 {
@@ -26,18 +34,28 @@ int main()
         die("connect() error");
     }
 
-    char msg[] = "hello";
-    write(fd, msg, strlen(msg));
-
-    char rbuf[64] = {};
-    ssize_t n = read(fd, rbuf, sizeof rbuf - 1);
-    if (n == -1) {
-        die("read() error");
+    // multiple requests
+    int32_t err = query(fd, "hello1");
+    if (err) {
+        goto L_DONE;
     }
-    printf("server says: %s\n", rbuf);
-    close(fd);
+    err = query(fd, "hello2");
+    if (err) {
+        goto L_DONE;
+    }
+    err = query(fd, "hello3");
+    if (err) {
+        goto L_DONE;
+    }
 
+    L_DONE:
+    close(fd);
     return 0;
+}
+
+static void msg(const char *msg)
+{
+    fprintf(stderr, "%s\n", msg);
 }
 
 static void die(const char *msg)
@@ -45,4 +63,81 @@ static void die(const char *msg)
     int err = errno;
     fprintf(stderr, "[%d] %s\n", err, msg);
     abort();
+}
+
+static int32_t query(int fd, const char *text)
+{
+    uint32_t len = (uint32_t) strlen(text);
+    if (len > k_max_msg) {
+        return -1;
+    }
+
+    char wbuf[4 + k_max_msg];
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], text, len);
+    if (int32_t err = write_all(fd, wbuf, 4 + len)) {
+        return err;
+    }
+
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if (err) {
+        if (errno == 0) {
+            msg("EOF");
+        } else {
+            msg("read() error");
+        }
+        return err;
+    }
+
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // reply body
+    err = read_full(fd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    rbuf[4 + len] = '\0';
+    printf("server says: %s\n", &rbuf[4]);
+    return 0;
+}
+
+static int32_t read_full(int fd, char *buf, size_t n)
+{
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            // unexpected EOF 该怎么理解呢?
+            // error, or unexpected EOF
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n)
+{
+    while (n > 0) {
+        ssize_t  rv = write(fd, buf, n);
+        if (rv <= 0) {
+            // 返回 0 也算有问题吗?
+            return -1; // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -5,10 +5,15 @@
 #include <unistd.h>
 #include <cstring>
 #include <cerrno>
+#include <cassert>
 
-static void do_something(int connfd);
+const size_t k_max_msg = 4096;
+
 static void msg(const char *msg);
 static void die(const char *msg);
+static int32_t one_request(int connfd);
+static int32_t read_full(int fd, char *buf, size_t n);
+static int32_t write_all(int fd, const char *buf, size_t n);
 
 int main()
 {
@@ -43,7 +48,13 @@ int main()
             continue; // error
         }
 
-        do_something(connfd);
+        // only serve one client connection at once
+        while (true) {
+            int32_t err = one_request(connfd);
+            if (err) {
+                break;
+            }
+        }
         close(connfd);
     }
 
@@ -62,16 +73,75 @@ static void die(const char *msg)
     abort();
 }
 
-static void do_something(int connfd)
+static int32_t read_full(int fd, char *buf, size_t n)
 {
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof rbuf - 1);
-    if (n < 0) {
-        msg("read() error");
-        return;
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            // unexpected EOF 该怎么理解呢?
+            // error, or unexpected EOF
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
     }
-    printf("client says: %s\n", rbuf);
+    return 0;
+}
 
-    char wbuf[] = "world";
-    write(connfd, wbuf, strlen(wbuf));
+static int32_t write_all(int fd, const char *buf, size_t n)
+{
+    while (n > 0) {
+        ssize_t  rv = write(fd, buf, n);
+        if (rv <= 0) {
+            // 返回 0 也算有问题吗?
+            return -1; // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t one_request(int connfd)
+{
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err) {
+        if (errno == 0) {
+            msg("EOF");
+        } else {
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4); // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    // reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[ 4 + sizeof(reply)];
+    len = (uint32_t) strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
 }
